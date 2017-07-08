f3a4736a39c90ab1b2a7abf1e8d4d26c24488dc6
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-08 12:00:24 +0200

    add model song and artist

---
+++ db/migrate/20170708095936_create_artists.rb
@@ -0,0 +1,12 @@
+class CreateArtists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.text :bio
+      t.references :song, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Songs.destroy_all
+Artists.destroy_all
+
+artist1 =
+artist2 =
+artist3 =
+
+song1 =
+song2 =
+song3 =
+song4 =
+song5 =
