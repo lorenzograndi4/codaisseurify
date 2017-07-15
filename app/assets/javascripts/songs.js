235d811f38b618a326bb617f6153191c6d0c0c7c
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-15 15:21:32 +0200

    add new song jquery only

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,3 +1,32 @@
+function createSong(name, year) {
+
+  var songBlock = $("<div class='song-block'></div>");
+
+  var titleBlock = $("<h3 class='song-title'></h3>");
+  titleBlock.html(name);
+
+  var deleteLink = $("<a href='#' class='delete-song'></a>");
+  deleteLink.html("[New delete link]");
+
+  var detailBlock = $("<p></p>");
+  detailBlock.html("Year: " + year + " ");
+
+  songBlock.append(titleBlock);
+  detailBlock.append(deleteLink);
+  songBlock.append(detailBlock);
+
+  $("#songslist").append(songBlock);
+}
+
+function submitSong(event) {
+  event.preventDefault();
+  var name = $("#new-song-name").val();
+  var year = $("#new-song-year").val();
+  createSong(name, year);
+  $("#new-song-name").val(null);
+  $("#new-song-year").val(null);
+}
+
 function removeSong(event) {
   event.preventDefault();
   $(this).parent().parent().remove()
@@ -9,7 +38,7 @@ function removeAllSongs(event) {
 }
 
 $(document).ready(function() {
-  // $("form").bind('submit', submitSong);
+  $("form").bind('submit', submitSong);
   $(".delete-song").bind('click', removeSong);
-  $("#remove-all").bind('click', removeAllSongs);
+  $("#delete-all").bind('click', removeAllSongs);
 });
